ee44ed65bac3b46bc04608e6fd36794e007681ea
gitreja <51505109+gitreja@users.noreply.github.com>
2022-06-02 10:53:14 +0530

    Update App.js
    test msg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> TESTING GITHUB ACTIONS WITH REACT </h1>        
+        <h1> RESHMA TESTING GITHUB ACTIONS WITH REACT </h1>        
         <a
           className="App-link"
           href="https://reactjs.org"
